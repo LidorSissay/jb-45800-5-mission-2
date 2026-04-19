1c5b83b3599c2495c65b7b6298cce5e812ca7757
LidorSissay <lidsis96w@gmail.com>
2026-04-19 12:32:22 +0300

    created input field and a button in Home component

--- src/components/03-home/Home.tsx
+++ src/components/03-home/Home.tsx
@@ -1,9 +1,12 @@
+import Title from '../06-title/Title'
 import './Home.css'
 
 const Home = () => {
     return (
         <div className="Home">
-            Home
+            <Title title="Home" />
+            <input placeholder='Enter a word...' />
+            <button>Translate</button>
         </div>
     )
 }
