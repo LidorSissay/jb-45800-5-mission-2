e9ea55e647254c15f1940a3375f88c32b9e325e2
LidorSissay <lidsis96w@gmail.com>
2026-04-19 14:20:15 +0300

    About component changed

--- src/components/05-about/About.tsx
+++ src/components/05-about/About.tsx
@@ -1,9 +1,12 @@
+import Title from '../06-title/Title'
 import './About.css'
 
 const About = () => {
     return (
         <div className="About">
-            About
+            <Title title="About" />
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis debitis sunt quos accusantium laborum inventore voluptates, officiis aut, error dolor beatae delectus labore vel nostrum.</p>
+            <p>Lidor Sissay</p>
         </div>
     )
 }
